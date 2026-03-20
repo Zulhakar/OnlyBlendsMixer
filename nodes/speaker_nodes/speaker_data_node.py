c1dd49f4951c66776bed7ffbab0ceae91f78edd6
Zulhakar <Ilineiros@proton.me>
2026-03-20 13:59:15 +0100

    speaker_data: fix blender crash after node is removed, global msg_bus in obc

--- nodes/speaker_nodes/speaker_data_node.py
+++ nodes/speaker_nodes/speaker_data_node.py
@@ -1,23 +1,29 @@
 import bpy
 from ...nodes.mixer_node import ObmSoundNode
-
+from ...cnt.base.global_data import Data
+from ...config import IS_DEBUG
+import uuid
 
 def strip_frame_start_end_changed(*args):
     self = args[0]
     strip = args[1]
-    self.outputs[0].input_value = strip.frame_end - strip.frame_start
+    if self:
+        self.outputs[0].input_value = strip.frame_end - strip.frame_start
 
 
 class SpeakerDataNode(ObmSoundNode,  bpy.types.Node):
     '''Speaker Data Node to get data from Speaker'''
     bl_label = "Speaker Data"
     bl_icon = 'OUTLINER_DATA_SPEAKER'
+    uuid_msg_bus: bpy.props.StringProperty()
+
 
     def init(self, context):
         self.inputs.new('NodeSocketSpeaker', "Speaker")
         self.outputs.new('NodeSocketFloatCnt', "Strip Length")
         self.outputs.new('NodeSocketFloatCnt', "Volume")
         self.outputs.new('NodeSocketFloatCnt', "Pitch")
+        self.uuid_msg_bus = str(uuid.uuid4()).replace("-", "")
         super().init(context)
 
     def get_speaker_data(self):
@@ -31,23 +37,26 @@ class SpeakerDataNode(ObmSoundNode,  bpy.types.Node):
 
     def free(self):
         super().free()
-        bpy.msgbus.clear_by_owner(self)
+        bpy.msgbus.clear_by_owner(Data.uuid_message_bus[self.uuid_msg_bus])
+        del Data.uuid_message_bus[self.uuid_msg_bus]
 
     def speaker_update(self):
         speaker = self.inputs[0].input_value
         if speaker is not None and speaker.name in bpy.data.objects:
             strip = bpy.data.objects[speaker.name].animation_data.nla_tracks["SoundTrack"].strips[0]
+            msg_bus_obj = object()
+            Data.uuid_message_bus[self.uuid_msg_bus] = msg_bus_obj
             bpy.msgbus.subscribe_rna(
                 key=strip.path_resolve("frame_end_ui", False),
                 # key=(bpy.types.NlaStrip, "frame_end_ui"),
-                owner=self,
+                owner=msg_bus_obj,
                 args=(self, strip,),
                 notify=strip_frame_start_end_changed,
                 options={'PERSISTENT'}
             )
             bpy.msgbus.subscribe_rna(
                 key=strip.path_resolve("frame_start_ui", False),
-                owner=self,
+                owner=msg_bus_obj,
                 args=(self, strip,),
                 notify=strip_frame_start_end_changed,
                 options={'PERSISTENT'}
@@ -64,6 +73,7 @@ class SpeakerDataNode(ObmSoundNode,  bpy.types.Node):
                 link.to_socket.input_value = self.outputs[0].input_value
 
     def refresh_outputs(self):
-        self.log("refresh_outputs")
+        if IS_DEBUG:
+            self.log("refresh")
         self.speaker_update()
         self.get_speaker_data()
